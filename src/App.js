63cd9f4c08b38f40cc3eedbe5e42ccb2a482cc4d
totaparis <totaparis@gmail.com>
2022-11-11 22:55:51 -0600

    Roll Dice v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+import DiceImage1 from "./images/Dice1.png";
+import DiceImage2 from "./images/Dice2.png";
+import DiceImage3 from "./images/Dice3.png";
+import DiceImage4 from "./images/Dice4.png";
+import DiceImage5 from "./images/Dice5.png";
+import DiceImage6 from "./images/Dice6.png";
+
+function App() 
+{
+  var diceImages = [
+    DiceImage1,
+    DiceImage2,
+    DiceImage3,
+    DiceImage4,
+    DiceImage5,
+    DiceImage6
+  ]
+
+  const [ image, setNewImage] = useState(diceImages[0])
+  const [ image2, setNewImage2] = useState(diceImages[1])
+
+  const rollDice = () =>{
+    var randomNum1 = Math.floor(Math.random() * 6)
+    var randomNum2 = Math.floor(Math.random() * 6)
+    setNewImage(diceImages[randomNum1]);
+    setNewImage2(diceImages[randomNum2]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <h1> Bienvenido a la App de Dados</h1>
+        <div className='container'>
+          <img className='square' src={image}></img>
+          <div style={{width: '5px',display: 'inline-block'}}></div>
+          <img className='square' src={image2}></img>
+        </div>
+        <button type='button' class="btn btn-outline-success" onClick={rollDice}>Tirar los dados</button>
+      </center>
     </div>
   );
 }
